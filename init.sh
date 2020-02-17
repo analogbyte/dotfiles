9b5b6ccf65a4b3a3373c96f09391010bb50da364
Daniel Nägele <code@danieln.de>
2020-02-17 14:45:56 +0100

    migrate to sway/wayland

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ mkdir -p ~/.backup
 
 dotfiles_dir="$( cd "$(dirname "$0")" && pwd )"
 
-for pkg in dunst fish git i3 nvim screen termite tmux xorg zathura; do
+for pkg in fish git nvim sway termite zathura; do
     stow -d ${dotfiles_dir} -t ${HOME} -R ${pkg}
 done;
