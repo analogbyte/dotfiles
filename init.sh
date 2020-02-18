d4cfd55d2a7155a2e2a38e3cb2b91179e0ae860d
Daniel Nägele <code@danieln.de>
2020-02-18 09:46:30 +0100

    add pam env to dotifles

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ mkdir -p ~/.backup
 
 dotfiles_dir="$( cd "$(dirname "$0")" && pwd )"
 
-for pkg in fish git nvim sway termite zathura; do
+for pkg in fish git nvim pam_environment sway termite zathura; do
     stow -d ${dotfiles_dir} -t ${HOME} -R ${pkg}
 done;
